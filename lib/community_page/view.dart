c065177efadc5f971a5251aeb708dc0071022345
rezaa <reza_naufal21@apps.ipb.ac.id>
2020-10-22 00:26:11 +0700

    add Community adder

--- lib/community_page/view.dart
+++ lib/community_page/view.dart
@@ -7,45 +7,112 @@ class CommunityPage extends StatefulWidget {
 }
 
 class _CommunityPageState extends State<CommunityPage> {
+  TextEditingController _c = new TextEditingController();
+  List<Widget> _post = [
+    _buildCardPost(
+        description:
+            "Pernah gak sih anak kalian tiba tiba sakit malam-malam padahal siangnya gk kenapa-kenapa dan gk bilang apa - apa, masih main bola dan main pesawat-pesawatan sama teman-teman dan ayahnya yang sekarang"),
+    _buildCardPost(
+        description:
+            "Suami saya sangat sedih anak saya di diagnosa downsyndrome, ada yang sama seperti saya tidak ya? ada yang punya saran cara menghiburnya gk ya?"),
+    _buildCardPost(
+        description:
+            "Anak saya namanya Nora dia kelebihan kromosom karena kita melahirkannya di umur yang sudah tua renta dan gawat jika melahirkan anak lagi, apakah ada yang kasusnya sama seperti saya?"),
+    _buildCardPost(
+        description:
+            "Anak saya namanya Nora dia kelebihan kromosom karena kita melahirkannya di umur yang sudah tua renta dan gawat jika melahirkan anak lagi, apakah ada yang kasusnya sama seperti saya?"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Community"),
-        actions: [
-          IconButton(
-              icon: Icon(
-                Icons.add,
-                color: Colors.white,
-              ),
-              onPressed: null)
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.only(left: 16, right: 16),
-          child: Column(
-            children: [
-              SizedBox(height: 16),
-              _buildCard(
-                  "Pernah gak sih anak kalian tiba tiba sakit malam-malam padahal siangnya gk kenapa-kenapa dan gk bilang apa - apa, masih main bola dan main pesawat-pesawatan sama teman-teman dan ayahnya yang sekarang"),
-              SizedBox(height: 4),
-              _buildCard(
-                  "Suami saya sangat sedih anak saya di diagnosa downsyndrome, ada yang sama seperti saya tidak ya? ada yang punya saran cara menghiburnya gk ya?"),
-              SizedBox(height: 4),
-              _buildCard(
-                  "Anak saya namanya Nora dia kelebihan kromosom karena kita melahirkannya di umur yang sudah tua renta dan gawat jika melahirkan anak lagi, apakah ada yang kasusnya sama seperti saya?"),
-              SizedBox(height: 4),
-              _buildCard(
-                  "Anak saya namanya Nora dia kelebihan kromosom karena kita melahirkannya di umur yang sudah tua renta dan gawat jika melahirkan anak lagi, apakah ada yang kasusnya sama seperti saya?"),
-            ],
-          ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Community"),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  showDialog(
+                      child: new Dialog(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height - 570,
+                          child: new Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: new TextField(
+                                  decoration: InputDecoration(
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Colors.black,
+                                      ),
+                                      borderRadius: const BorderRadius.all(
+                                        const Radius.circular(10.0),
+                                      ),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Colors.black,
+                                      ),
+                                      borderRadius: const BorderRadius.all(
+                                        const Radius.circular(10.0),
+                                      ),
+                                    ),
+                                  ),
+                                  controller: _c,
+                                ),
+                              ),
+                              new FlatButton(
+                                child: new Text("Post"),
+                                onPressed: () {
+                                  setState(() {
+                                    _post.add(
+                                      _buildCardPost(description: _c.text),
+                                    );
+                                    _c.text = "";
+                                  });
+                                  Navigator.of(context, rootNavigator: true)
+                                      .pop(context);
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      context: context);
+                })
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Column(
+                children: List<Widget>.generate(_post.length, (int index) {
+                  return Column(
+                    children: [
+                      SizedBox(height: 4),
+                      _post[_post.length - index - 1],
+                    ],
+                  );
+                }),
+              )),
         ),
       ),
     );
   }
+}
 
-  Widget _buildCard(String description) {
+class _buildCardPost extends StatelessWidget {
+  final String description;
+
+  const _buildCardPost({Key key, this.description}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       child: Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
